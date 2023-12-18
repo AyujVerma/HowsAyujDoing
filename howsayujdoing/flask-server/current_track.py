baefa9c456790a17a03203a48962fa5fc3149a99
AyujVerma <ayuj.verma@tagmagnet.org>
2023-12-18 15:09:07 -0600

    Current status of project: diplaying current song on the website. I now need to work on song analysis.

---
+++ howsayujdoing/flask-server/current_track.py
@@ -0,0 +1,91 @@
+import os
+import requests
+from pprint import pprint # Prints dictionaries nicely.
+from dotenv import load_dotenv # Loads .env file.
+import json
+
+# Load environment variables from the .env file
+load_dotenv('auth.env')
+
+SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+SPOTIFY_ACCESS_TOKEN = os.getenv('SPOTIFY_ACCESS_TOKEN')
+SPOTIFY_REFRESH_TOKEN = os.getenv('SPOTIFY_REFRESH_TOKEN')
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+def get_access_token():
+    auth_url = 'https://accounts.spotify.com/api/token'
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': SPOTIFY_REFRESH_TOKEN,
+        'client_id': SPOTIFY_CLIENT_ID,
+        'client_secret': SPOTIFY_CLIENT_SECRET,
+    }
+
+    response = requests.post(auth_url, data=data)
+    return response.json().get('access_token')
+
+def get_current_track(access_token):
+    response = requests.get(
+        SPOTIFY_GET_CURRENT_TRACK_URL,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+    # If the access token has expired, refresh it and retry the request.
+    if response.status_code == 401:
+        new_access_token = get_access_token()
+        response = requests.get(
+            SPOTIFY_GET_CURRENT_TRACK_URL,
+            headers={
+                'Authorization': f'Bearer {new_access_token}',
+            },
+        )
+    current_info = {
+        "type": None,
+        "id": None,
+        "name": None,
+        "link": None,
+        "duration": None,
+        "progress": None,
+        "image": None,
+        "artists": None,
+    }
+    try:
+        resp_json = response.json()
+        current_info['type'] = resp_json['currently_playing_type'] # Track, episode, ad, or none
+        if current_info['type'] == 'track' or current_info['type'] == 'episode':
+            current_info['id'] = resp_json['item']['id']
+            current_info['name'] = resp_json['item']['name']
+            current_info['link'] = resp_json['item']['external_urls']['spotify']
+            current_info['duration'] = resp_json['item']['duration_ms']
+            current_info['progress'] = resp_json['progress_ms']
+            current_info['image'] = resp_json['item']['album']['images'][0]['url']
+            if current_info['type'] == 'track':
+                artists = resp_json['item']['artists'] # Array of artist objects.
+                artists_names = ', '.join(
+                    [artist['name'] for artist in artists]
+                )
+                current_info['artists'] = artists_names
+        elif current_info['type'] == 'ad':
+            current_info['name'] = 'Advertisement'
+            current_info['image'] = 'https://drive.google.com/uc?id=1dDSsjkDUX_2OM3iFTXxC1POubWCZObzH'
+            current_info['artists'] = ['Coporate America']
+        else:
+            current_info['name'] = 'Offline'
+            current_info['image'] = 'https://drive.google.com/uc?id=1U-vVukBvPqdjw23i1GTMSC_zXLR08soX'
+            current_info['artists'] = ['Ayuj']
+
+    except json.JSONDecodeError as e:
+        # Handle the case where JSON decoding fails (e.g., when Spotify is closed)
+        print(f"Error decoding JSON response: {e}")
+
+        # Set default values for current_info
+        current_info['name'] = 'Offline'
+        current_info['image'] = 'https://drive.google.com/uc?id=1U-vVukBvPqdjw23i1GTMSC_zXLR08soX'
+        current_info['artists'] = ['Ayuj']
+
+    except Exception as e:
+        print(f"Error processing response: {e}")
+    return current_info
--- howsayujdoing/src/App.js
+++ howsayujdoing/src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
-function App() {
+const ENDPOINT = 'http://localhost:5000';  // Change to Flask server's address
+
+const App = () => {
+  const [currentInfo, setCurrentInfo] = useState({
+    type: null,
+    id: null,
+    name: null,
+    link: null,
+    duration: null,
+    progress: null,
+    image: null,
+    artists: null,
+  });
+  const moods = ['Joy', 'Trust', 'Fear', 'Surprise', 'Sadness', 'Anticipation', 'Anger', 'Disgust']; // Taken from Plutchik's eight basic emotions
+  const [mood, setMood] = useState('');
+  const [innerSquareColor, setInnerSquareColor] = useState('');
+  const [prevTrackInfo, setPrevTrackInfo] = useState(null);
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+
+    socket.on('response', (data) => {
+      console.log('Socket response:', data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/current-track');
+        const data = await response.json();
+        setCurrentInfo(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+    const intervalTime = 15 * 1000; // Default: 15 seconds
+    const refreshInterval = setInterval(fetchData, intervalTime);
+
+    return () => {
+      clearInterval(refreshInterval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (prevTrackInfo && currentInfo.id === prevTrackInfo.id) 
+    {
+      return;
+    }
+
+    switch (currentInfo.type) {
+      case 'track':
+        const randomMood = getRandomMood();
+        const randomHexColor = getRandomHexColor();
+        setMood(randomMood);
+        setInnerSquareColor(randomHexColor);
+        break;
+      case 'ad':
+        setMood('Impatient');
+        setInnerSquareColor('#D3D3D3');
+        break;
+      default:
+        setMood('Asleep');
+        setInnerSquareColor('#999AC6');
+    }
+    
+    setPrevTrackInfo(currentInfo);
+  }, [currentInfo, prevTrackInfo]);
+
+  const getRandomMood = () => {
+    const randomIndex = Math.floor(Math.random() * moods.length);
+    return moods[randomIndex];
+  };
+
+  const getRandomHexColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="outer-square" style={{ backgroundColor: innerSquareColor }}>
+        <div className="inner-square">
+          <img src={currentInfo.image} className="album-cover" alt="Album Cover" />
+        </div>
+        <div className="text-below">
+          <div className="title">
+            <label>{currentInfo.name}</label>
+          </div>
+          <div className="artists">
+            <div>{currentInfo.artists}</div>
+          </div>
+          <div className="mood">
+            <div>Mood: {mood}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
